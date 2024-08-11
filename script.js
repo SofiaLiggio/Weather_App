beff6fd74ca4da95acd0b5bf1ec4021826abb02c
sofia_liggio <sofialiggio994@gmail.com>
2024-08-11 17:38:55 +0200

    add more script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".container");
 const search = document.querySelector(".search-box button");
 const weatherBox = document.querySelector(".weather-box");
 const weatherDetails = document.querySelector(".weather-details");
+const error404 = document.querySelector(".not-found");
 
 search.addEventListener("click", () => {
   const APIKey = "98740f4ebc0d63bc0f8ba70090e5a091";
@@ -10,10 +11,23 @@ search.addEventListener("click", () => {
   if (city == "") return;
 
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}"
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
   )
     .then((response) => response.json())
     .then((json) => {
+      if (json.cod == "404") {
+        container.style.height = "400px";
+        weatherBox.classList.remove("active");
+        weatherDetails.classList.remove("active");
+        error404.classList.add("active");
+        return;
+      }
+
+      container.style.height = "555px";
+      weatherBox.classList.add("active");
+      weatherDetails.classList.add("active");
+      error404.classList.remove("active");
+
       const image = document.querySelector(".weather-box img");
       const temperature = document.querySelector(".weather-box temperature");
       const description = document.querySelector(".weather-box description");
@@ -49,14 +63,10 @@ search.addEventListener("click", () => {
 
         default:
           image.src = "./images/cloud.png";
-
-      switch (key) {
-        case value:
-          break;
-
-        default:
-          break;
-
       }
+      temperature.innerHTML = `${parseInt(json.main.temp)} <span>°C</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
     });
 });
